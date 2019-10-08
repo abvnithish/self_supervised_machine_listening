3fc2cf86b0a934f5919d9b68c0628622f18a0d04
B V Nithish Addepalli <abvnithish@gmail.com>
2019-10-07 21:58:14 -0400

    Initial soundNet time reversal codes

---
+++ code/get_log_scalograms.py
@@ -0,0 +1,60 @@
+
+### Importing Libraries
+import os
+import time
+import random
+from glob import glob
+import numpy as np
+import pandas as pd
+import librosa
+from librosa.display import specshow
+import multiprocessing as mp
+
+ROOT_DIR = '/beegfs/bva212/fma_small/'
+PATH_FILE = '/checksums'
+SR = 22050
+
+
+filenames = []
+dump = []
+if os.path.exists(ROOT_DIR + PATH_FILE):
+    with open(ROOT_DIR + PATH_FILE) as fp:
+        lines = fp.readlines()
+        for line in lines:
+            track_id, file_name = line.split()
+            if os.path.exists(ROOT_DIR + file_name):
+                filenames.append(file_name)
+print(f'There are a total of {len(filenames)} music files in the root directory')
+
+def save_waveforms(file):
+
+    ROOT_DIR = '/beegfs/bva212/fma_small/'
+    waveform, fs_ = librosa.load((ROOT_DIR + file), sr = SR)
+    waveform_flipped = np.ascontiguousarray(np.flip(waveform), dtype=np.float32)
+
+    cqt = librosa.cqt(waveform)
+    cqt_flipped = librosa.cqt(waveform_flipped)
+
+    logscalogram = librosa.amplitude_to_db(np.abs(cqt))
+    logscalogram_flipped = librosa.amplitude_to_db(np.abs(cqt_flipped))
+
+    np.save((ROOT_DIR + file + '_cqt.npy'), logscalogram)
+    np.save((ROOT_DIR + file + '_cqt_flipped.npy'), logscalogram_flipped)
+
+    return True
+
+
+start = time.time()
+with mp.Pool(os.cpu_count()) as p:
+    results = p.map_async(save_waveforms, filenames[100:200])
+    output = results.get()
+print(f'\nTime for loading files is {time.time() - start}')
+
+
+
+
+
+
+
+
+
