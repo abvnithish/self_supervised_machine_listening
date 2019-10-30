d1eaac7cc6ddba5e0481daa0a34d60cad46e0dca
Shreyas <shreyas.chandrakaladharan@gmail.com>
2019-10-30 17:42:03 -0400

    adding snet2_jigsaw

--- code/jigsaw/models.py
+++ code/jigsaw/models.py
@@ -46,6 +46,8 @@ def load_model(device, model_arch , mode, separable_falg):
         model = snet_flip().to(device)
     elif model_name == 'snet_jigsaw':
         model = snet_jigsaw().to(device)
+    elif model_name == 'snet2_jigsaw':
+        model = snet2_jigsaw().to(device)
     elif model_name == 'snet_jigsaw_separable':
         model = snet_jigsaw_separable().to(device)
     elif model_name == 'l3net_jigsaw':
@@ -161,7 +163,72 @@ class snet_flip(nn.Module):
         logits = self.mlp_layer(out.view(out.shape[0], -1))
         return logits
 
+class snet2_jigsaw(nn.Module):
 
+    def __init__(self):
+        '''
+        Create the 5 Conv Layer Sound Net network architecture as per the paper - https://arxiv.org/pdf/1610.09001.pdf
+        '''
+        super(snet2_jigsaw, self).__init__()
+
+        self.conv_layers = nn.Sequential(nn.Conv2d(in_channels = 1, out_channels= 16, kernel_size = 5, stride = 2, padding = 5), 
+                                nn.BatchNorm2d(num_features = 16), 
+                                nn.ReLU(inplace = True),
+
+                                nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size = 5, stride = 2, padding = 5),
+                                nn.BatchNorm2d(32),
+                                nn.ReLU(inplace = True),
+
+                                nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 5, padding = 5),
+                                nn.BatchNorm2d(64),
+                                nn.ReLU(inplace = True),
+                                nn.AvgPool2d(kernel_size = 3),
+
+                                nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 4, padding = 4),
+                                nn.BatchNorm2d(128),
+                                nn.ReLU(inplace = True),
+
+                                nn.Conv2d(in_channels = 128, out_channels = 256, kernel_size = 4, padding = 4),
+                                nn.BatchNorm2d(256),
+                                nn.ReLU(inplace = True),
+                                nn.AvgPool2d(kernel_size = 3),
+
+                                nn.Conv2d(in_channels = 256, out_channels = 512, kernel_size = 3, stride = 2, padding = 3),
+                                nn.BatchNorm2d(512),
+                                nn.ReLU(inplace = True),
+
+                                nn.Conv2d(in_channels = 512, out_channels = 1024, kernel_size = 3, stride = 2, padding = 3),
+                                nn.BatchNorm2d(1024),
+                                nn.ReLU(inplace = True),
+                                nn.AdaptiveAvgPool2d(output_size = 1)
+                                )
+        self.concat_mlp_layer = nn.Sequential(nn.Linear(3072, 2048),
+                                              nn.BatchNorm1d(num_features = 2048), 
+                                              nn.ReLU(inplace = True),
+                                              
+                                              nn.Linear(2048, 1024),
+                                              nn.BatchNorm1d(num_features = 1024), 
+                                              nn.ReLU(inplace = True),
+                                              
+                                              nn.Linear(1024, 256),
+                                              nn.BatchNorm1d(num_features = 256), 
+                                              nn.ReLU(inplace = True),
+                                             )
+        self.mlp_layer = nn.Linear(256, 2)
+              
+    def forward(self, input):
+        conv_strips = []
+        n_strips = input.shape[1]
+        for strip in range(n_strips):
+            conv_strip = input[:,strip]
+            conv_strip = conv_strip.unsqueeze(1)
+            conv_strips.append(self.conv_layers(conv_strip))
+
+        concat_out=torch.cat(conv_strips,1)
+        out = self.concat_mlp_layer(concat_out.view(concat_out.shape[0], -1))
+        output = self.mlp_layer(out.view(out.shape[0], -1))
+        return output
+    
 class snet_jigsaw(nn.Module):
 
     def __init__(self):
